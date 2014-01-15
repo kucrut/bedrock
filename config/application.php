285d95e1ef513a0085ffb4c165d0ede054d842cb
Dzikri Aziz <kvcrvt@gmail.com>
2014-01-15 09:00:27 +0700

    Move DB_* definitions to application.php

--- config/application.php
+++ config/application.php
@@ -16,6 +16,11 @@ define('WP_ENV', getenv('WP_ENV') ? getenv('WP_ENV') : 'development');
 
 $env_config = dirname(__FILE__) . '/environments/' . WP_ENV . '.php';
 
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
+
 if (file_exists($env_config)) {
   require_once $env_config;
 }
