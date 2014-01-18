57817a4da28903336056d9a6184e3abab5a23630
Dzikri Aziz <kvcrvt@gmail.com>
2014-01-19 04:51:11 +0700

    VVV init: Improve reinit

--- bin/vvv-init.sh
+++ bin/vvv-init.sh
@@ -13,9 +13,13 @@ if [[ 'vagrant' != "${USER}" ]] || [[ 'vvv' != "${HOST}" ]]; then
 	exit 1
 fi
 
+# Formatting
 T_BOLD=$(tput bold)
 T_NORMAL=$(tput sgr0)
 T_UNDERSCORE=$(tput smul)
+
+# Defaults
+REINIT=false
 
 while :
 do
@@ -32,6 +36,10 @@ Options:
 
 HELP
 			exit 0      # This is not an error, User asked help. Don't do "exit 1"
+			;;
+		-r | --reinit)
+			REINIT=true
+			shift
 			;;
 		#-f | --file)
 		#	# You might want to check if you really got FILE
@@ -62,8 +70,9 @@ source $ENV_FILE
 
 # Get sitename
 SITENAME=$(echo $WP_HOME | sed 's/^http\(\|s\):\/\///g')
+DONE_MESSAGE="\n${T_BOLD}${SITENAME}${T_NORMAL} is ready!\n"
 
-echo -e "\n## Provisioning ${SITENAME} ##"
+echo -e "\n## Provisioning ${T_BOLD}${SITENAME}${T_NORMAL} ##\n"
 
 # Get domain names
 if [ -z "${DOMAIN_NAMES}" ]; then
@@ -71,7 +80,7 @@ if [ -z "${DOMAIN_NAMES}" ]; then
 fi
 
 # Create database, if we don't already have one
-echo " * Creating database '${DB_NAME}'";
+echo " * Creating database ${T_BOLD}${DB_NAME}${T_NORMAL}";
 SQL="CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';"
 SQL="${SQL} GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@${DB_HOST} IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -u root --password=root -e "${SQL}"
@@ -118,59 +127,63 @@ for DOMAIN_NAME in $DOMAIN_NAMES; do
 done
 echo -e $HOST_NAMES > $VVV_HOSTS_FILE
 
-# Manual invocation
-if [ -n "$1" ] && [[ "-r" = "$1" ]]; then
-	DIR_NAME=$(basename $BASEDIR)
-	echo -e "\nThe script was manually invoked. It will try to replace the existing nginx config for this site, but no promises :P";
-
-	NGINX_CONFIG_DIR="/etc/nginx/custom-sites"
-	OLD_CONFIGS=$(find $NGINX_CONFIG_DIR -name "*${DIR_NAME}*")
-	if [ -n "$OLD_CONFIGS" ]; then
-		echo " * Old Nginx config found and moved to /tmp:"
-		for FILE_NAME in $OLD_CONFIGS; do
-			echo "   * /tmp/$(basename ${FILE_NAME})"
-		done
-		sudo mv $OLD_CONFIGS /tmp
-	fi
-
-	NEW_CONFIG="${NGINX_CONFIG_DIR}/${DIR_NAME}.conf"
-	sudo cp $NGINX_CONF $NEW_CONFIG
-	echo " * New Nginx config added: ${NEW_CONFIG}"
-
-	echo " * Checking host names:"
-	VVV_IP=$(ip addr list eth1 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-	BORDER_LENGTH=0
-	while IFS='' read -r DOMAIN_NAME && [ -n "$DOMAIN_NAME" ]; do
-		if [[ "#" != ${DOMAIN_NAME:0:1} ]] && [[ "*" != ${DOMAIN_NAME:0:1} ]]; then
-			echo "   * ${DOMAIN_NAME}"
-
-			NEW_ADD_TO_HOST="${VVV_IP} ${DOMAIN_NAME} # vvv-auto"
-			LENGTH=${#NEW_ADD_TO_HOST}
-			[ $LENGTH -gt $BORDER_LENGTH ] && BORDER_LENGTH=$LENGTH
-			ADD_TO_HOST="${ADD_TO_HOST}${NEW_ADD_TO_HOST}\n"
-
-			if ! $(grep -q "^127.0.0.1 ${DOMAIN_NAME}" /etc/hosts); then
-				NEW_HOSTS="${NEW_HOSTS}127.0.0.1 ${DOMAIN_NAME} # vvv-auto\n"
-			fi
-		fi
-	done < $VVV_HOSTS_FILE
-
-	if [ -n "${NEW_HOSTS}" ]; then
-		echo -e $NEW_HOSTS | sudo tee -a /etc/hosts > /dev/null
-	fi
-
-	if [ -n "${ADD_TO_HOST}" ]; then
-		BORDER_CHAR="="
-		BORDER_LENGTH=$((${BORDER_LENGTH} - 8))
-		BORDER=$(printf "%${BORDER_LENGTH}s" | sed "s/ /${BORDER_CHAR}/g" )
-		echo -e "\n*** Add the following lines to your HOST SYSTEM's /etc/hosts file ***\n"
-		echo -e "### ${BORDER} ###"
-		echo -e $ADD_TO_HOST | sed '/^$/d'
-		echo -e "### ${BORDER} ###\n"
-	fi
-
-	# Finally, restart nginx
-	sudo service nginx restart
+if ! $REINIT; then
+	echo -e ${DONE_MESSAGE}
+	exit 0;
 fi
 
-echo -e "\n${SITENAME} is ready!\n"
+# Reinit
+DIR_NAME=$(basename $BASEDIR)
+echo -e "\nThe script was manually invoked. It will try to replace the existing nginx config for this site, but no promises :P";
+
+NGINX_CONFIG_DIR="/etc/nginx/custom-sites"
+OLD_CONFIGS=$(find $NGINX_CONFIG_DIR -name "*${DIR_NAME}*")
+if [ -n "$OLD_CONFIGS" ]; then
+	echo " * Old Nginx config found and moved to /tmp:"
+	for FILE_NAME in $OLD_CONFIGS; do
+		echo "   * /tmp/$(basename ${FILE_NAME})"
+	done
+	sudo mv $OLD_CONFIGS /tmp
+fi
+
+NEW_CONFIG="${NGINX_CONFIG_DIR}/${DIR_NAME}.conf"
+sudo cp $NGINX_CONF $NEW_CONFIG
+echo " * New Nginx config added: ${NEW_CONFIG}"
+
+echo " * Checking host names:"
+VVV_IP=$(ip addr list eth1 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
+BORDER_LENGTH=0
+while IFS='' read -r DOMAIN_NAME && [ -n "$DOMAIN_NAME" ]; do
+	if [[ "#" != ${DOMAIN_NAME:0:1} ]] && [[ "*" != ${DOMAIN_NAME:0:1} ]]; then
+		echo "   * ${DOMAIN_NAME}"
+
+		NEW_ADD_TO_HOST="${VVV_IP} ${DOMAIN_NAME} # vvv-auto"
+		LENGTH=${#NEW_ADD_TO_HOST}
+		[ $LENGTH -gt $BORDER_LENGTH ] && BORDER_LENGTH=$LENGTH
+		ADD_TO_HOST="${ADD_TO_HOST}${NEW_ADD_TO_HOST}\n"
+
+		if ! $(grep -q "^127.0.0.1 ${DOMAIN_NAME}" /etc/hosts); then
+			NEW_HOSTS="${NEW_HOSTS}127.0.0.1 ${DOMAIN_NAME} # vvv-auto\n"
+		fi
+	fi
+done < $VVV_HOSTS_FILE
+
+if [ -n "${NEW_HOSTS}" ]; then
+	echo -e $NEW_HOSTS | sudo tee -a /etc/hosts > /dev/null
+fi
+
+if [ -n "${ADD_TO_HOST}" ]; then
+	BORDER_CHAR="="
+	BORDER_LENGTH=$((${BORDER_LENGTH} - 8))
+	BORDER=$(printf "%${BORDER_LENGTH}s" | sed "s/ /${BORDER_CHAR}/g" )
+	echo -e "\n*** Add the following lines to your HOST SYSTEM's /etc/hosts file ***\n"
+	echo -e "### ${BORDER} ###"
+	echo -e $ADD_TO_HOST | sed '/^$/d'
+	echo -e "### ${BORDER} ###\n"
+fi
+
+# Finally, restart nginx
+sudo service nginx restart
+
+echo -e ${DONE_MESSAGE}
+exit 0;
