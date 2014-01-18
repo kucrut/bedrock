73ea455b872bef0438d69efa398e6939ffcb9420
Dzikri Aziz <kvcrvt@gmail.com>
2014-01-18 08:40:55 +0700

    Deploy: Create & Upload .env if not exists yet

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,17 +17,33 @@ set :deploy_to, "/srv/www/#{fetch(:application)}"
 set :log_level, :info
 
 set :linked_files, %w{.env}
-set :linked_dirs, %w{app/uploads}
+set :linked_dirs, %w{root/app/uploads}
 
 namespace :deploy do
+
+  desc 'Create linked files'
+  namespace :check do
+    task :linked_files => '.env'
+  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # This task is required by Capistrano but can be a no-op
       # Your restart mechanism here, for example:
-      # execute :service, :nginx, :reload
+      #execute :sudo, :systemctl, :restart, :mysqld
+      #execute :sudo, :service, :mysql, :restart
     end
   end
 
+end
+
+remote_file '.env' => "#{Dir.pwd}/.env", roles: :app
+
+file "#{Dir.pwd}/.env" do |t|
+  if !FileTest.exist? "#{shared_path}/.env"
+    puts "\n\033[32mLet's create the \033[33m.env\033[32m file for \033[33m#{fetch(:stage)}\033[0m\n\n"
+    sh "composer run-script post-install-cmd"
+    puts "\n"
+  end
 end
