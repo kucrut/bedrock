a7838ca755deab69a140546aa5a0c4575646b8c1
Dzikri Aziz <kvcrvt@gmail.com>
2015-11-04 15:41:48 +0700

    Fix PHPCS issues

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,6 +1,6 @@
 <?php
 /* Development */
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
-define('SCRIPT_DEBUG', true);
-define('JETPACK_DEV_DEBUG', true);
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
+define( 'JETPACK_DEV_DEBUG', true );
--- config/environments/production.php
+++ config/environments/production.php
@@ -1,5 +1,5 @@
 <?php
 /* Production */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
