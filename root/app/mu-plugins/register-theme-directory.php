6d844227396190e8cb30e0cf3f3444909aee7f22
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-25 01:05:43 +0800

    Move themes install location to `root/app/themes-composer`

--- root/app/mu-plugins/register-theme-directory.php
+++ root/app/mu-plugins/register-theme-directory.php
@@ -1,3 +1,11 @@
 <?php
 
-register_theme_directory(ABSPATH . 'wp-content/themes/');
+/**
+ * Plugin Name: Custom theme directories
+ * Version: 0.1
+ * Author: Dzikri Aziz
+ * Author URI: http://kucrut.org
+ */
+
+register_theme_directory( ABSPATH . 'wp-content/themes' );
+register_theme_directory( WP_CONTENT_DIR . '/themes-composer' );
